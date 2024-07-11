299b6db1bc70067951efded7114cea9fd53c4a1d
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2024-07-11 18:16:08 -0300

    feat: add use-disclosure

--- src/use-disclosure.ts
+++ src/use-disclosure.ts
@@ -1,3 +1,32 @@
-export function useDisclosure() {
-  return { state: "open", onStateChange: () => {} };
+import { useCallback, useState } from "react";
+
+type Props = {
+  /**
+   * Define the initial state
+   */
+  initialState?: "opened" | "closed";
+};
+
+export function useDisclosure({ initialState = "closed" }: Props) {
+  const [state, setState] = useState(initialState);
+
+  const open = useCallback(() => setState("opened"), []);
+  const close = useCallback(() => setState("closed"), []);
+
+  const toggle = useCallback(() => {
+    if (state === "opened") {
+      setState("closed");
+    } else {
+      setState("opened");
+    }
+  }, [state]);
+
+  return {
+    state,
+    open,
+    close,
+    toggle,
+    isOpened: state === "opened",
+    isClosed: state === "closed",
+  };
 }
